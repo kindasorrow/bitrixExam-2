2a6349b852a5dfacf6c8901a4625457f573693d8
kindasorrow <whats_good@mail.ru>
2024-07-11 15:00:28 +0500

    [ex2-51]

--- www/local/php_interface/include/events.php
+++ www/local/php_interface/include/events.php
@@ -3,6 +3,7 @@ IncludeModuleLangFile(__FILE__);
 
 AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", array("Ex2", "OnBeforeProductUpdateHandler"));
 AddEventHandler("main", "OnEpilog", array("Ex2", "Error404Handler"));
+AddEventHandler("main", "OnBeforeEventAdd", array("Ex2", "OnBeforeEventAddHandler"));
 
 class Ex2
 {
@@ -13,14 +14,14 @@ class Ex2
         if ($arFields["IBLOCK_ID"] == IBLOCK_CATALOG) {
             if ($arFields["ACTIVE"] == "N") {
 
-                $arSelect = Array("ID", "NAME", "SHOW_COUNTER");
-                $arFilter = Array("IBLOCK_ID"=>IBLOCK_CATALOG, "ID" => $arFields["ID"]);
-                $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+                $arSelect = array("ID", "NAME", "SHOW_COUNTER");
+                $arFilter = array("IBLOCK_ID" => IBLOCK_CATALOG, "ID" => $arFields["ID"]);
+                $res = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
 
                 $ob = $res->Fetch();
                 echo $ob["SHOW_COUNTER"];
 
-                if($ob["SHOW_COUNTER"] > MAX_COUNT) {
+                if ($ob["SHOW_COUNTER"] > MAX_COUNT) {
                     $message = GetMessage("EX2_ERROR_DEACTIVATE_PRODUCT", array("#SHOW_COUNTER#" => $ob["SHOW_COUNTER"]));
                     $APPLICATION->throwException($message);
                     return false;
@@ -32,8 +33,9 @@ class Ex2
         return true;
     }
 
-    public static function Error404Handler() {
-        if(defined('ERROR_404') && ERROR_404 == "Y") {
+    public static function Error404Handler()
+    {
+        if (defined('ERROR_404') && ERROR_404 == "Y") {
             global $APPLICATION;
             $APPLICATION->RestartBuffer();
 
@@ -49,9 +51,40 @@ class Ex2
                     "AUTH_TYPE_ID" => "ERROR_404",
                     "MODULE_ID" => "main",
                     "DESCRIPTION" => $APPLICATION->GetCurPage(),
-                    )
+                )
             );
         }
+    }
+
+    public static function OnBeforeEventAddHandler(&$event, &$lid, &$arFields)
+    {
+        if ($event == "FEEDBACK_FORM") {
+            global $USER;
+            if ($USER->IsAuthorized()) {
+                $message = GetMessage("EX2_MAIL_AUTHORISED_USER", [
+                    '#ID#' => $USER->GetID(),
+                    '#NAME#' => $USER->GetFullName(),
+                    '#LOGIN#' => $USER->GetLogin(),
+                    '#AUTHOR#' => $arFields['AUTHOR']
+                ]);
+            }
+            else {
+                $message = GetMessage("EX2_MAIL_UNAUTHORISED_USER", ['#AUTHOR#' => $arFields['AUTHOR']]);
+            }
+            $arFields['AUTHOR'] = $message;
+
+            CEventLog::Add([
+                'SEVERITY' => 'INFO',
+                "AUDIT_TYPE_ID" => "MY_OWN_TYPE",
+                "MODULE_ID" => "main",
+                "ITEM_ID" => $event,
+                'USER_ID' => $USER->IsAuthorized() ? $USER->GetID() : NULL,
+                'DESCRIPTION' => GetMessage('EX2_CHANGED_MAIL_LOG', [
+                    '#AUTHOR#' => $message
+                ])
+            ]);
+        }
+
     }
 }
 
--- www/local/php_interface/include/lang/ru/events.php
+++ www/local/php_interface/include/lang/ru/events.php
@@ -1,2 +1,6 @@
 <?php
 $MESS["EX2_ERROR_DEACTIVATE_PRODUCT"] = "Товар невозможно деактивировать, у него #SHOW_COUNTER# просмотров";
+
+$MESS["EX2_MAIL_UNAUTHORISED_USER"] = "Пользователь не авторизован, данные из формы: #AUTHOR#";
+$MESS["EX2_MAIL_AUTHORISED_USER"] = "Пользователь авторизован #ID# (#LOGIN#) #NAME#, данные из формы: #AUTHOR#";
+$MESS["EX2_CHANGED_MAIL_LOG"] = "Замена данных в отсылаемом письме – #AUTHOR#";
