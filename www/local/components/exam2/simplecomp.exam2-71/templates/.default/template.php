1362f2e9cb4347c5ec8202a7ba6c51453f1980ab
kindasorrow <whats_good@mail.ru>
2024-07-15 12:18:19 +0500

    [ex2-71]

--- www/local/components/exam2/simplecomp.exam2-71/templates/.default/template.php
+++ www/local/components/exam2/simplecomp.exam2-71/templates/.default/template.php
@@ -1,11 +1,5 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 <p><b><?= GetMessage("SIMPLECOMP_EXAM2_CAT_TITLE") ?></b></p>
-<pre>
-<?
-print_r($arResult);
-?>
-</pre>
-
 <ul>
     <? foreach ($arResult['BRANDS'] as $arItem): ?>
         <li>
