9c61669ac889dc2c27c3fc4374144d93c6896917
komissarov <whats_good@mail.ru>
2024-07-16 13:06:12 +0500

    [ex2-81]

--- www/local/components/exam2/simplecomp.exam2/templates/.default/template.php
+++ www/local/components/exam2/simplecomp.exam2/templates/.default/template.php
@@ -1,11 +1,6 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 <p><b><?= GetMessage("SIMPLECOMP_EXAM2_CAT_TITLE") ?>:</b></p>
 <p><?=GetMessage("SIMPLECOMP_EXAM2_COUNT", ["#count#" => $arResult['PRODUCT_COUNT']])?></p>
-<pre>
-<?php
-print_r($arResult);
-?>
-</pre>
 <ul>
     <?php foreach ($arResult["NEWS"] as $news): ?>
         <li><b><?= $news["NAME"] ?></b> - <?= $news["ACTIVE_FROM"] ?> (<?= implode(', ', $news['SECTIONS']); ?>)
