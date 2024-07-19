d550572b931d8797224073e8979da211914e4625
kindasorrow <whats_good@mail.ru>
2024-07-19 19:44:21 +0500

    [ex2-88]

---
+++ www/ex2/time_control/index.php
@@ -0,0 +1,18 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>products/index.php - 18.02%
+
+
+</br>
+
+Работа компонента по умолчанию: bitrix:news.detail: 0.0356 с; Запросов: 5 (0.0021 с); кеш: 7 КБ
+
+</br>
+
+Работа компонента только с необходимым кешированием:bitrix: news.detail: 0.0386 с; Запросов: 6 (0.0026 с); кеш: 4 КБ
+
+</br>
+
+Разница в объемах кеша: 3 КБ
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
